3e05a4801071d766b1d19fcf1f92e02a0e98069d
Adam St Arnaud <ajstarna@ualberta.ca>
2022-09-21 21:24:04 -0600

    forgot to add messages file

---
+++ src/messages.rs
@@ -0,0 +1,54 @@
+use actix::prelude::{Message, Recipient};/// Game server sends this messages to session
+
+use uuid::Uuid;
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct WsMessage(pub String);
+
+/// Message for ws communications
+
+/// New ws session is created
+#[derive(Message)]
+#[rtype(result = "Uuid")]
+pub struct Connect {
+    pub addr: Recipient<WsMessage>,
+}
+
+/// Session is disconnected
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Disconnect {
+    pub id: Uuid,
+}
+
+/// Send message to specific table
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct ClientMessage {
+    /// Id of the client session
+    pub id: Uuid,
+    /// Peer message
+    pub msg: String,
+    /// Table name
+    pub table: String,
+}
+
+/// List of available tables
+pub struct ListTables;
+
+impl actix::Message for ListTables {
+    type Result = Vec<String>;
+}
+
+/// Join table, if table does not exists create new one.
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Join {
+    /// Client ID
+    pub id: Uuid,
+
+    /// Table name
+    pub name: String,
+}
+
