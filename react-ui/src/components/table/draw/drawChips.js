389dc435f468d6214eef7cd49f9546d7528b6ef9
Michael Feist <mdfeist@ualberta.ca>
2023-03-17 19:41:48 -0600

    Add chips stacking and colors to better represent the value.

--- react-ui/src/components/table/draw/drawChips.js
+++ react-ui/src/components/table/draw/drawChips.js
@@ -1,5 +1,29 @@
 import { roundRect } from "./drawFunctions";
 
+function drawChip(ctx, x, y, chip_size, color) {
+    // Draw chips
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.strokeStyle = "black";
+    ctx.arc(x, y+0.1*chip_size, chip_size, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.strokeStyle = "black";
+    ctx.arc(x, y, chip_size, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "black";
+    ctx.arc(x, y, 0.6*chip_size, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.stroke();
+}
+
 export function drawChips(ctx, value, x, y, w, h, size) {
     let chip_size = 0.15*size;
 
@@ -17,24 +41,18 @@ export function drawChips(ctx, value, x, y, w, h, size) {
     ctx.fillText(value, x-w/2 + 0.3*size, y-h/2 + 0.2*size);
 
     // Draw chips
-    ctx.beginPath();
-    ctx.fillStyle = "yellow";
-    ctx.strokeStyle = "black";
-    ctx.arc(x-w/2, y+0.1*chip_size, chip_size, 0, Math.PI * 2, true);
-    ctx.fill();
-    ctx.stroke();
+    const chipValues = [1000, 500, 100, 25, 20, 10, 5, 1];
+    const chipColors = ["maroon", "purple", "black", "green", "yellow", "orange", "red", "white"];
 
-    ctx.beginPath();
-    ctx.fillStyle = "yellow";
-    ctx.strokeStyle = "black";
-    ctx.arc(x-w/2, y, chip_size, 0, Math.PI * 2, true);
-    ctx.fill();
-    ctx.stroke();
+    let remaining = value;
+    let y0 = y;
+    for (let i = 0; i < 8; i++) {
+        let chips = Math.floor(remaining/chipValues[i]);
+        remaining = value % chipValues[i];
 
-    ctx.beginPath();
-    ctx.fillStyle = "white";
-    ctx.strokeStyle = "black";
-    ctx.arc(x-w/2, y, 0.6*chip_size, 0, Math.PI * 2, true);
-    ctx.fill();
-    ctx.stroke();
+        for (let j = 0; j < chips; j++) {
+            drawChip(ctx, x-w/2, y0, chip_size, chipColors[i]);
+            y0 -= 0.4*chip_size;
+        }
+    }
 }
